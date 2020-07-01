8996629b788aa4437678a510d00b6f687c2d3b88
prabhatkadam18 <prabhatkadam18@gmail.com>
2020-07-01 19:28:59 +0530

    first commit

---
+++ src/main/java/EPAM/Calculator/Calculator.java
@@ -0,0 +1,7 @@
+package EPAM.Calculator;
+
+abstract class Calculator
+{
+    double first_operand,second_operand;
+    abstract double calculate();
+}
---
+++ src/main/java/EPAM/Calculator/Division.java
@@ -0,0 +1,15 @@
+package EPAM.Calculator;
+
+public class Division extends Calculator
+{
+    Division(double a, double b)
+    {
+        first_operand = a;
+        second_operand = b;
+    }
+
+    double calculate()
+    {
+        return first_operand/second_operand;
+    }
+}
